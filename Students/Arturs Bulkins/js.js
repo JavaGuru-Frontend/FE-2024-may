e9a08367b223298b2bf65b0c6823122d66892705
Ar4xs <arturs.bulkins@gmail.com>
2024-06-19 13:27:55 +0300

    end of 3rd lesson

---
+++ Students/Arturs Bulkins/js.js
@@ -0,0 +1,3 @@
+/*document.getElementById('zeka')
+    <button id="zeka" class="btn btn__full">find gift now<button>
+document.getElementById('zeka').classList="btn btn_danger">find gift now<button>*/
