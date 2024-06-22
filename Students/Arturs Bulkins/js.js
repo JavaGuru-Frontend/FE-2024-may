7888743e01d67ad7b5989ae9d1c4f49eae10bf62
Ar4xs <arturs.bulkins@gmail.com>
2024-06-22 16:01:02 +0300

    lesson n4

--- Students/Arturs Bulkins/js.js
+++
@@ -1,3 +0,0 @@
-/*document.getElementById('zeka')
-    <button id="zeka" class="btn btn__full">find gift now<button>
-document.getElementById('zeka').classList="btn btn_danger">find gift now<button>*/
