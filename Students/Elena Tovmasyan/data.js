78df894b8f4418c2bebfc7f53fa4730deb3f6a64
ElenaTovmasyan <bioteo237@gmail.com>
2024-07-18 19:01:28 +0300

    JS Start

--- Students/Elena Tovmasyan/data.js
+++ Students/Elena Tovmasyan/data.js
@@ -1,3 +1,89 @@
 let num1 = 10;
 let num2 = 20;
-console.log (num1 + num2);
+let result = function(num1, num2) {
+    console.log(num1 + num2)}
+
+
+let webpage {
+    header = {
+        logo = {url: img/logo.png},
+        navigation: [
+            link1 = {
+                name: `Home`,
+                url: `#`
+            },
+            link2 = {
+                name: `About Us`,
+                url: `#`
+            },
+            link3 = {
+                name: `Services`,
+                url: `#`
+            },
+            link4 = {
+                name: `Blog`,
+                url: `#`
+            }
+        ],
+        btn = {
+            name: `GIFT FINDER`,
+            url: `#`
+        }
+    },
+    main = { 
+        heroblock = {
+            text1: `Find a gift for your wife or girlfriend`,
+            text2: `Struggling to find a gift for your wife or girlfriend? No problem!`,
+            btn1 = {
+                name: `Find gift now`,
+                url: `#`
+            },
+            img1 = {url: `../img/bg1.png`},
+            img2 = {url: `../img/bg2.png`},
+            text3: `We’ve actually already asked hundreds of women what they want, and made a database with the results.
+                All you have to do is take our short survey that only takes seconds, and our proprietary algorithm will match you with a few perfect gift options.`,
+            btn2 = {
+                name: `Find gift no`,
+                url: `#`
+            }
+        } 
+
+        title1 = {
+            text1: `Our Best Blogs Ever`,
+            text2: `“Try our blog to find the best tips and tricks to select your gift”`
+        }
+
+        gifts = {
+
+        }
+    }
+}
+
+
+
+/* let data = {
+    navigation {
+        links: [
+            {
+                name: 'linkName',
+                url: 'linkUrl'
+            },
+            {
+                name: 'linkName',
+                url: 'linkUrl'
+            }
+        ]
+    },
+    footer {
+        links: [
+            {
+                name: 'linkName',
+                url: 'linkUrl'
+            },
+            {
+                name: 'linkName',
+                url: 'linkUrl'
+            }
+        ]
+    }
+} */
