1b162f7bdbd344c5ee92fba9c18f7b8e164f2ae8
ElenaTovmasyan <bioteo237@gmail.com>
2024-07-25 11:35:03 +0300

    JS Equal Problem

--- Students/Elena Tovmasyan/Calculator/main.js
+++ Students/Elena Tovmasyan/Calculator/main.js
@@ -1,13 +1,14 @@
-let printValue = () => {
-    document.getElementById(`IN`).innerHTML = ugfklj;
+let printValue = (printNumber) => {
+    document.getElementById(`IN`).innerHTML += `${printNumber}`; 
 }
 
+/* printValue()
+ */
 
+/* первый способ, как вызвать нажатием кнопки появление контента; = `${printNumber}` - цифра перезаписывается,
+    += - если мы хотим, чтобы цифра не перезаписывалась, а добавлялась к предыдущей */
 
-
-let name = 'Уолтер'
-let surname = 'Вайт'
-let fullName = name + ' ' + surname
-
-console.log(fullName)
-
+let equal = () => {
+    console.log(
+        eval(document.getElementById(`IN`).innerHTML));
+}
