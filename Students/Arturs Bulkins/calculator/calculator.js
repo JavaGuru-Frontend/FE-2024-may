684f74c5ff883661c2e8b21c431d4d5880a0a773
Ar4xs <arturs.bulkins@gmail.com>
2024-08-01 11:34:46 +0300

    lesson 9

--- Students/Arturs Bulkins/calculator/calculator.js
+++ Students/Arturs Bulkins/calculator/calculator.js
@@ -1,7 +1,89 @@
-let display = document.getElementById("output");
+let btnArray = document.getElementsByClassName("btn");
+let output = document.getElementById('output');
+
+/* for (let i = 0; i < btnArray.length; i++) {
+    btnArray[i].addEventListener('click', () => {
+        console.log('test')
+    })
+} */
+let printValue = (input, clearInput) => {
+    output.innerHTML += `${input}`; 
+}
+
+let clearInput = () => {
+    output.innerHTML = '';
+}
+
+let handleError = () => {
+    console.error(`ERROR`);
+}
+
+let equal = () => { 
+    let result = eval(output.innerHTML);
+    clearInput ();
+    printValue (result);
+   
+}
 
 
-let printValue = (input) => {
+
+
+Array.from(btnArray).forEach((element) => {
+    element.addEventListener('click', () =>{
+        let type = element.getAttribute('data-type');
+        let id = element.getAttribute('data-id');
+
+        switch (type) {
+            case 'equal':
+                equal(id);
+                break;
+            
+            case 'operator':
+                printValue(id);
+                break;
+            
+            case 'number':
+                printValue(id);
+                break;
+
+            case 'symbol':
+                printValue(id);
+                break;   
+
+            case 'clear':
+                clearInput();
+                break;        
+
+            default:
+                break;
+        }
+    })
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode !== 13) {
+        for(let i = 0; i < btnArray.length; i++) {
+            let id = btnArray[i].getAttribute('data-id');
+            if(id === event.key) {
+               btnArray[i].click();
+            }
+        }
+    } else {
+        equal();
+    }
+    });   
+
+    
+
+ 
+
+
+
+
+
+
+
+/* let printValue = (input) => {
     display.innerHTML += `${input}`;
 }
    
@@ -19,4 +101,4 @@ calculate = () => {
             display.innerHTML = "ERROR"
             break; 
         }}
-
+ */
