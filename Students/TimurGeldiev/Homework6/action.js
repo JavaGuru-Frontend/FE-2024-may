964fb481aea6b8867cbebf5a9d1e6f3ba697fb1d
GTimur <timur.geldiev@28stone.com>
2024-11-19 21:37:19 +0200

    Homework6

---
+++ Students/TimurGeldiev/Homework6/action.js
@@ -0,0 +1,7 @@
+const logoPath = template1.header.logo;
+const headerLogo = document.getElementById("headerLogo");
+headerLogo.src = logoPath;
+
+const logoOrange = template1.footer.logo;
+const footerLogo = document.getElementById("logoOrange");
+footerLogo.src = logoOrange;
---
+++ Students/TimurGeldiev/Homework6/data.js
@@ -0,0 +1,14 @@
+let template1 = {
+    header:{
+        logo: "../template1/images/Logo.png",
+        links: ["Home", "AboutUs", "Services", "Blog"],
+        btn: "Gift finder"
+    },
+    footer: {
+        logo: "../template1/images/logoOrange.png",
+        links:["Home", "About", "Services", "Blog"],
+        socialMediaLinks: ["FB", "X", "Instagram", "Youtube", "Pinterest"
+        ]
+
+    }
+}
