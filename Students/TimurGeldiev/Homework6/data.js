32ef55bdba78d4bdfdfe1d86c63ba70d42f85611
GTimur <timur.geldiev@28stone.com>
2024-11-19 21:47:45 +0200

    HW6Upd

--- Students/TimurGeldiev/Homework6/data.js
+++ Students/TimurGeldiev/Homework6/data.js
@@ -1,14 +1,53 @@
 let template1 = {
-    header:{
-        logo: "../template1/images/Logo.png",
-        links: ["Home", "AboutUs", "Services", "Blog"],
-        btn: "Gift finder"
-    },
-    footer: {
-        logo: "../template1/images/logoOrange.png",
-        links:["Home", "About", "Services", "Blog"],
-        socialMediaLinks: ["FB", "X", "Instagram", "Youtube", "Pinterest"
-        ]
+  header: {
+    logo: "../template1/images/Logo.png",
+    links: [
+      {
+        title: "Home",
+        url: "#",
+      },
+      {
+        title: "AboutUs",
+        url: "#",
+      },
+      {
+        title: "Services",
+        url: "#",
+      },
+      {
+        title: "Blog",
+        url: "#",
+      },
+    ],
 
-    }
-}
+    btn: "Gift finder",
+  },
+  footer: {
+    logo: "../template1/images/logoOrange.png",
+    links: [
+      {
+        title: "Home",
+        url: "#",
+      },
+      {
+        title: "AboutUs",
+        url: "#",
+      },
+      {
+        title: "Services",
+        url: "#",
+      },
+      {
+        title: "Blog",
+        url: "#",
+      },
+    ],
+    socialMediaLinks: [
+      { title: "FB", url: "#" },
+      { title: "X", url: "#" },
+      { title: "Instagram", url: "#" },
+      { title: "Youtube", url: "#" },
+      { title: "Pinterest", url: "#" },
+    ],
+  },
+};
