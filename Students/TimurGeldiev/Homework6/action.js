630f4d26321cb3a633578912830e446011be1e9f
GTimur <timur.geldiev@28stone.com>
2024-11-19 22:42:58 +0200

    HW6Upd2

--- Students/TimurGeldiev/Homework6/action.js
+++ Students/TimurGeldiev/Homework6/action.js
@@ -1,7 +1,11 @@
-const logoPath = template1.header.logo;
-const headerLogo = document.getElementById("headerLogo");
-headerLogo.src = logoPath;
+let navigation = document.getElementById("navigation");
+template1.header.links.forEach(element=>{
+    navigation.innerHTML += `<a href="${element.url}" class="header__link">${element.title}</a>`
+});
 
-const logoOrange = template1.footer.logo;
-const footerLogo = document.getElementById("logoOrange");
-footerLogo.src = logoOrange;
+let headerLogo = document.getElementById("headerLogo");
+headerLogo.src = template1.header.logo;
+
+
+let footerLogo = document.getElementById("logoOrange");
+footerLogo.src = template1.footer.logo;
