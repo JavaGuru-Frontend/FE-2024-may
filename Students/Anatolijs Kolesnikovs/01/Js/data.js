a64d17f961ebcfd1702c4978b3e7b308407baff3
Y031k <bmbacardi@gmail.com>
2024-11-21 16:27:29 +0200

    js_first try

---
+++ Students/Anatolijs Kolesnikovs/01/Js/data.js
@@ -0,0 +1,73 @@
+let webpage = {
+    header: {
+        logo: "img/logo.png",
+        links: [
+            {
+                title:'Home',
+                url: '#'
+            },
+            {
+                title:'About us',
+                url: '#'
+            },
+            {
+                title:'Services',
+                url: '#'
+            },
+            {
+                title:'Blog',
+                url: '#'
+            }
+        ],
+        btn: 
+            {
+                title:'GIFT FINDER',
+                url: '#'
+            },
+
+            },
+    footer: {
+        links: [
+            {
+                title: 'Home',
+                url: '#',
+            },
+            {
+                title: 'About us',
+                url: '#',
+            },
+            {
+                title: 'Services',
+                url: '#',
+            },
+            {
+                title: 'Blog',
+                url: '#',
+            },
+        ],
+        social: [
+            {
+               icon: 'icon-fb',
+               url: 'http://www.google.com',
+            },
+            {
+                icon: 'icon-in',
+                url: '#',
+             },
+             {
+                icon: 'icon-pn',
+                url: '#',
+             },
+             {
+                icon: 'icon-tw',
+                url: '#',
+             },
+             {
+                icon: 'icon-yt',
+                url: '#',
+             },
+        ]
+    }
+
+
+}
