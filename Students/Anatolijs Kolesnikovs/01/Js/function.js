3d847cd940b57dd357105d8ea1728a3e72554c1c
Y031k <bmbacardi@gmail.com>
2024-11-24 19:24:54 +0200

    TEST

--- Students/Anatolijs Kolesnikovs/01/Js/function.js
+++ Students/Anatolijs Kolesnikovs/01/Js/function.js
@@ -6,8 +6,8 @@ webpage.header.links.forEach(element => {
     nav.innerHTML += `<a href="${element.url}" class="header__link">${element.title}</a>`
 });
 
-// let btn = document.getElementById("btn");
-// webpage.header.btn.innerHTML += `<button class="header__btn btn">"${element.title}"</button>`;
+let btn = document.getElementById("btn");
+webpage.header.btn.innerHTML += `<button class="header__btn btn">"${element.title}"</button>`;
 
 let footer__nav = document.getElementById("footer__nav");
 webpage.footer.links.forEach(element => {
