9a39a8cd51a67bda68400dca1398c8294870c939
GTimur <timur.geldiev@28stone.com>
2024-11-25 11:42:49 +0200

    Homework7CalculatorUpd

--- Students/TimurGeldiev/Homework7/calculator.js
+++ Students/TimurGeldiev/Homework7/calculator.js
@@ -1,68 +1,79 @@
-let inElement = document.getElementById('input');
-let allBtn = document.getElementsByClassName('btn');
+let inElement = document.getElementById("input");
+let allBtn = document.getElementsByClassName("btn");
 
 let isResultShown = false;
 
-Array.from(allBtn).forEach(button => {
-    button.addEventListener('click', () => {
-        let buttonValue = button.textContent;
-        let operators = ["+", "-", "*", "/"];
-        let lastChar = inElement.value[inElement.value.length - 1];
+Array.from(allBtn).forEach((button) => {
+  button.addEventListener("click", () => {
+    let buttonValue = button.textContent;
+    let operators = ["+", "-", "*", "/"];
+    let lastChar = inElement.value[inElement.value.length - 1];
 
-        if (buttonValue != "=" && inElement.value.length < 10) {
-            if (isResultShown){
+    if (buttonValue != "=" && inElement.value.length < 10) {
+      if (isResultShown) {
+        inElement.value = "";
+        isResultShown = false;
+      }
+      switch (buttonValue) {
+        case "×":
+          buttonValue = "*";
+          break;
+        case "÷":
+          buttonValue = "/";
+          break;
+        case "−":
+          buttonValue = "-";
+          break;
+        case "+":
+          buttonValue = "+";
+          break;
+        default:
+      }
+      if (operators.includes(buttonValue) && operators.includes(lastChar)) {
+        return;
+      }
+
+      inElement.value += buttonValue;
+    }
+    switch (buttonValue) {
+      case "=":
+        if (inElement.value == "" ||
+            operators.includes(lastChar) ||
+            operators.includes(inElement.value[0])) {
+                alert("Incorrect value");
                 inElement.value = "";
                 isResultShown = false;
-            }
-            switch (buttonValue) {
-                case "×":
-                    buttonValue = "*";
-                    break;
-                case "÷":
-                    buttonValue = "/";
-                    break;
-                case "−":
-                    buttonValue = "-";
-                    break;
-                case "+":
-                    buttonValue= "+";
-                    break;
-                default:
-                                    
-            }
-            if (operators.includes(buttonValue) && operators.includes(lastChar)) {
-                return;
-            }
-            
-            inElement.value += buttonValue;
+          return;
+        } else if (inElement.value.includes("/0")) {
+          alert("Can't divide by 0");
+          inElement.value = "";
+          isResultShown = false;
+          return;
+        } else {
+          try {
+            inElement.value = eval(inElement.value);
+            isResultShown = true;
+          } catch (error) {
+            console.error("Eval function error", error);
+          }
         }
-        switch(buttonValue){
-            case "=":   
-            if(operators.includes(lastChar)){
-                return;
-            }else{     
-                inElement.value = eval(inElement.value); 
-                isResultShown = true;             
-            }
-            case "C":
-                inElement.value = "";
-                isResultShown = false;
-                break;
-            default:
-                break;
-        }
-        
-    })
+        break;
+      case "C":
+        inElement.value = "";
+        isResultShown = false;
+        break;
+      default:
+        break;
+    }
+  });
 });
 
-inElement.addEventListener('input', (event) => {
-    let inputValue = inElement.value;
-    let allowedChars = /^[0-9e+\-*/]*$/;
+inElement.addEventListener("input", (event) => {
+  let inputValue = inElement.value;
+  let allowedChars = /^[0-9e+\-*/]*$/;
 
-    if(!allowedChars.test(inputValue)){
-        alert("Wrong symbol");
-        inElement.value = inputValue.slice(0, -1);
-    }
-
-})
-
+  if (!allowedChars.test(inputValue)) {
+    alert("Wrong input");
+    inElement.value = "";
+  }
+});
