e0bc94a5e2474d1e2e3121afb19f5de91790085d
GTimur <timur.geldiev@28stone.com>
2024-12-08 12:35:28 +0200

    Homework7 Calc v2.0

--- Students/TimurGeldiev/Homework7/calculator.js
+++ Students/TimurGeldiev/Homework7/calculator.js
@@ -6,101 +6,138 @@ let isResultShown = false;
 const defaultValue = "0";
 inElement.value = defaultValue;
 
-Array.from(allBtn).forEach((button) => {
-  button.addEventListener("click", () => {
-    let buttonValue = button.innerHTML;
-    let operators = ["+", "-", "*", "/", "."];
-    let lastChar = inElement.value[inElement.value.length - 1];
-    
-
-    if (buttonValue != "=" && inElement.value.length < 10) {
-      if (isResultShown) {   //Очистка поля ввода при вводе нового числа если функция eval уже была использована
-        inElement.value = defaultValue;
-        isResultShown = false;
-      }
-      switch (buttonValue) { //switch для преобразования операторов для функции eval
-        case "×":
-          buttonValue = "*";
-          break;
-        case "÷":
-          buttonValue = "/";
-          break;
-        case "−":
-          buttonValue = "-";
-          break;
-        case "+":
-          buttonValue = "+";
-          break;
-        default:
-      }
-      if (operators.includes(buttonValue) && operators.includes(lastChar)) { //Проверка на ввод нескольких операторов подряд
-        return;
-      }
-      if(buttonValue == "0"){
-        if (inElement.value == "" || operators.includes(lastChar)){ //Если поле пустое или последний символ это оператор, то можно добавить ещё один 0
-          inElement.value += buttonValue;
-          return;
-        }
-        else if(inElement.value == "0"){ //Если в поле ввода только 0, то ещё один 0 добавить нельзя
-          return;
-        }
-      }
-      if(buttonValue == "." && lastChar == "0"){ //Ввод дробного числа
-        inElement.value += buttonValue;
-        return;
-      }
-      if(inElement.value == defaultValue && !operators.includes(buttonValue)){ //Перезапись дефолтного 0 на новое значение
-        inElement.value = buttonValue;
-      }else{
-        inElement.value += buttonValue;
-      }
-
-      
+let printValue = (value, replace = false) => {
+    if (replace) {
+        inElement.value = `${value}`;
+    } else {
+        inElement.value += `${value}`;
     }
-    switch (buttonValue) {
-      case "=":
-        if (inElement.value == "" || //если пустое значение поля
-            operators.includes(lastChar) || //если оператор последний в строке
-            operators.includes(inElement.value[0]) ) { //если оператор единственный в строке
-                alert("Incorrect value");
-                inElement.value = defaultValue;
-                isResultShown = false;
-          return;
-        } else if (inElement.value.includes("/0")) {  //Проверка деления на 0
-          alert("Can't divide by 0");
-          inElement.value = defaultValue;
-          isResultShown = false;
-          return;
-        } else {
-          try {
-            let result = eval(inElement.value);
-            if (Number.isInteger(result)){  //Проверка на целое число, если float то проиходит округление до 8 символа чтобы не выйти за границу поля
-              inElement.value = result;
-            }else{
-              inElement.value = result.toFixed(8);
-            }
-            isResultShown = true;
-          } catch (error) {
-            console.error("Eval function error", error);
-          }
-        }
-        break;
-      case "C": //Очистка поля 
-        inElement.value = defaultValue;
-        isResultShown = false;
-        break;
-      default:
-        break;
-    }
-  });
-});
-
-inElement.addEventListener("input", (event) => { 
-  let inputValue = inElement.value;
-  let allowedChars = /^[0-9e+\-*/.]*$/; //Разрешённые для ввода мануально символы
-
-  if (!allowedChars.test(inputValue)) {
-    alert("Wrong input");
+}
+let clearInput = () => {
     inElement.value = defaultValue;
-  }
+}
+let equal = () => {
+    try {
+        let result = eval(inElement.value);
+        if (isNaN(result) || !isFinite(result)) {
+            alert("Calculations error");
+            clearInput();
+        } else {
+            if (result % 1 !== 0) {
+                result = result.toFixed(8);
+            }
+            clearInput();
+            printValue(result, true);
+        }
+        isResultShown = true;
+
+    } catch (error) {
+        console.log(error);
+        clearInput();
+        printValue("ERROR", true);
+    }
+}
+let handleInput = (event) => {
+    let inputValue = inElement.value;
+    let allowedChars = /^[0-9e+\-*/.]*$/;
+    if (event.type === 'input') {
+        if (!allowedChars.test(inputValue)) {
+            alert("Wrong input");
+            clearInput();
+        }
+    }
+    if (event.type === 'keydown') {
+        let allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '.', 'e'];
+        if (allowedKeys.includes(event.key)) {
+            for (let i = 0; i < btnArray.length; i++) {
+                let id = btnArray[i].getAttribute('data-id');
+                if (id === event.key) {
+                    btnArray[i].click()
+                }
+            }
+        } else if (event.keyCode === 13) {
+            equal();
+        }
+    }
+}
+
+Array.from(allBtn).forEach((button) => {
+    button.addEventListener("click", () => {
+        let type = button.getAttribute('data-type');
+        let buttonValue = button.getAttribute('data-id');
+
+
+        let operators = ["+", "-", "*", "/", "."];
+        let lastChar = inElement.value[inElement.value.length - 1];
+
+
+        if (buttonValue !== "=") { //Очистка поля ввода при вводе нового числа если функция eval уже была использована
+            if (isResultShown) {
+                if (!operators.includes(buttonValue)) { //Если следующий ввод это оператор, то поле не очищается
+                    clearInput();
+                }
+                    isResultShown = false;
+
+
+            }
+
+            if (operators.includes(buttonValue) && operators.includes(lastChar)) { //Проверка на ввод нескольких операторов подряд
+                return;
+            }
+            if (buttonValue === ".") {
+                let numbers = inElement.value.split(/[\+\-\*\/]/);
+                let currentNumber = numbers[numbers.length - 1];
+                if (currentNumber.includes(".")) {
+                    return;
+                }
+                printValue(buttonValue);
+                return;
+            }
+            if (buttonValue === "0") {
+                if (inElement.value === "" || operators.includes(lastChar)) { //Если поле пустое или последний символ это оператор, то можно добавить ещё один 0
+                    printValue(buttonValue);
+                    return;
+                } else if (inElement.value === "0") { //Если в поле ввода только 0, то ещё один 0 добавить нельзя
+                    return;
+                }
+            }
+        }
+
+        switch (type) {
+            case 'operator':
+                if (!operators.includes(lastChar)) {
+                    printValue(buttonValue);
+                }
+                break;
+
+            case 'number':
+                if (inElement.value === defaultValue && buttonValue !== ".") {
+                    inElement.value = buttonValue;
+                } else if (inElement.value.length < 10) {
+                    printValue(buttonValue);
+                }
+                break;
+            case 'clear':
+                clearInput();
+                break;
+            case 'equal':
+                try {
+                    equal();
+                } catch (error) {
+                    console.log(error)
+                    clearInput();
+                }
+                break;
+
+            default:
+                alert("Something went wrong!");
+                break;
+        }
+
+    });
 });
+
+inElement.addEventListener("input", handleInput);
+inElement.addEventListener("keydown", handleInput);
+
+
