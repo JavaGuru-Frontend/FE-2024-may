57483f4c3708e0ff9a008ed996eb557cb8c383f8
Y031k <bmbacardi@gmail.com>
2024-12-18 18:05:11 +0200

    rebuild 2.0

--- Students/Anatolijs Kolesnikovs/07/js/data.js
+++ Students/Anatolijs Kolesnikovs/07/js/data.js
@@ -1,3 +1,4 @@
 
 let btnArray = document.getElementsByClassName('btn');
 let output = document.getElementById('output') ;
+let history = document.getElementById('history');
--- Students/Anatolijs Kolesnikovs/07/js/helpers.js
+++ Students/Anatolijs Kolesnikovs/07/js/helpers.js
@@ -10,12 +10,20 @@ let handleError = () => {
 	console.error('pizdec')
 }
 
+let saveHistory = (text) => {
+	localStorage.setItem('item', text);
+}
 
+let printHistory = () =>{
+	history.innerHTML = localStorage.getItem('item')
+}
 
 
 
 let equal = () => {
 	let result = eval(output.innerHTML);
+	saveHistory(`${output.innerHTML}=${result}`);
 	clearInput();
 	printValue(result);
+	printHistory()
 }
