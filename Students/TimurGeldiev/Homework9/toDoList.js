0805849c3daa81a04184379a8ba624c0bf54a0bb
GTimur <timur.geldiev@28stone.com>
2025-01-14 19:58:56 +0200

    Homework9 To-Do list edit function remove function checkedItem function

--- Students/TimurGeldiev/Homework9/toDoList.js
+++ Students/TimurGeldiev/Homework9/toDoList.js
@@ -10,33 +10,82 @@ let addTask = () => {
     printTasks();
 }
 
+let toggleDone = (event) => {
+    let clickedElement = event.target;
+    let index = clickedElement.dataset.index;
+    let tasks = JSON.parse(localStorage.getItem('taskList') || []);
+    tasks[index].done = !tasks[index].done;
+    localStorage.setItem('taskList', JSON.stringify(tasks));
+    if(clickedElement.classList.contains('todo_done')) {
+        clickedElement.classList.remove('todo_done');
+    }else{
+        clickedElement.classList.add('todo_done');
+    }
+
+}
+let removeTask = (index) => {
+    let tasks = JSON.parse(localStorage.getItem("taskList") || []);
+    if (index < 0 || index >= tasks.length) {
+        console.error(`Task not found!`);
+        return;
+    }
+    tasks.splice(index, 1);
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+    printTasks();
+}
+let editTask = (index) => {
+    let tasks = JSON.parse(localStorage.getItem("taskList") || []);
+    let currentTask = tasks[index];
+    let newText = prompt("Edit task:", currentTask.taskText);
+    if(newText !== null && newText.trim() !== ""){
+        currentTask.taskText = newText.trim();
+        tasks[index] = currentTask;
+        localStorage.setItem("taskList", JSON.stringify(tasks));
+        printTasks();
+    }
+}
 
 let saveTasks = (taskText) => {
-    let taskData = JSON.parse(localStorage.getItem('taskList')) || []
-    taskData.push(taskText);
+    const task = {
+        'done' : false,
+        'taskText' : taskText
+    }
+
+    let taskData = JSON.parse(localStorage.getItem('taskList')) || [] ;
+    taskData.push(task);
     localStorage.setItem('taskList', JSON.stringify(taskData));
 }
 let printTasks = () => {
-    fetch('https://dummyjson.com/todos')
-    .then(res => res.json())
-    .then(response => renderFromServer(response.todos));
+    // fetch('https://dummyjson.com/todos')
+    // .then(res => res.json())
+    // .then(response => renderFromServer(response.todos));
 
+    let printTask = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList.innerHTML = '';
+    printTask.forEach((taskRecord, index) => {
+        let isTaskChecked = taskRecord.done;
+        taskList.innerHTML += `<div class="itemContainer">
+                                <div class="todo ${isTaskChecked ? 'todo_done': ''} task_${index}"
+                                data-index="${index}" 
+                                onclick="toggleDone(event)"> ${index + 1}. ${taskRecord.taskText}
+                                </div>
+                                <div class="buttonsContainer">
+                                <button class="remove-btn" id="remove_${index}" onclick="removeTask(${index})">Remove</button>
+                                <button class="edit-btn" onclick="editTask(${index})">Edit</button>
+                                </div>
+                                </div>`
 
-    // let printTask = JSON.parse(localStorage.getItem('taskList'));
-    // taskList.innerHTML = '';
-    // printTask.forEach((taskRecord, index) => {
-    //     taskList.innerHTML += `<div>${index + 1}. ${taskRecord}</div>`
-    // })
+    })
 }
 let clearHistory = () => {
     localStorage.removeItem('taskList');
     taskList.innerHTML = '';
 }
-let renderFromServer = (todos) => {
-    todos.forEach((taskRecord) => {
-            taskList.innerHTML += `<div>${taskRecord.id}. ${taskRecord.todo}</div>`
-        })
-}
+// let renderFromServer = (todos) => {
+//     todos.forEach((taskRecord) => {
+//             taskList.innerHTML += `<div>${taskRecord.id}. ${taskRecord.todo}</div>`
+//         })
+// }
 
 addBtn.addEventListener("click", addTask);
 clearBtn.addEventListener("click", clearHistory);
