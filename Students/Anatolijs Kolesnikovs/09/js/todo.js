66a265e8a7c81f9f22b0915e4e42ce06358f779a
Y031k <bmbacardi@gmail.com>
2025-01-15 18:07:43 +0200

    todo update

--- Students/Anatolijs Kolesnikovs/09/js/todo.js
+++ Students/Anatolijs Kolesnikovs/09/js/todo.js
@@ -4,9 +4,24 @@ let outputElement = document.getElementById('output');
 
 
 let addValue = () => {
-outputElement.innerHTML += `<li>${(taskElement.value)}</li>`;
+saveToLocalStorage(taskElement.value);
+loadFromLocalStorage();
+}
 
+let saveToLocalStorage = (historyRecord) => {
+    let dataBase  = JSON.parse(localStorage.getItem('toDoList')) || [] ;
+    dataBase.push(historyRecord);
+    localStorage.setItem('toDoList', JSON.stringify(dataBase))
+}
+
+let loadFromLocalStorage = () => {
+    let dataBase  = JSON.parse(localStorage.getItem('toDoList')) || [] ;
+    dataBase.forEach(todo => {
+        outputElement.innerHTML += `<li>${todo}</li>`;
+    });
 
 }
 
 addBtnElement.addEventListener('click', addValue)
+
+loadFromLocalStorage();
